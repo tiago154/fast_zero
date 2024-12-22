8a443893373c60a2d667009b11db8ac0bfc319c6
Tiago Silva <tiago.toya@hotmail.com>
2024-12-22 19:15:29 -0300

    Add schema validation in api with pydantic

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello World'}
+
+
+@app.get('/hello_html', status_code=HTTPStatus.OK)
+def hello_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'hello World'}
+
+
+def test_hello_html_deve_retornar_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/hello_html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello World</h1>' in response.text
